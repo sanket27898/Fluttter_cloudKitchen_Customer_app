28cbad185e5c3cf2fdfca3c54f201e4fb31ac9e0
sanket <jainsanket784@gmail.com>
2021-01-21 17:25:42 +0530

    v10_flutter_appbar_with_bottom_search_bar here we make homescreen appbar with search textField or bar..'

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
@@ -12,6 +14,57 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthProvider>(context);
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        leading: Container(),
+        title: FlatButton(
+          onPressed: () {},
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Delivery Address',
+                style: TextStyle(color: Colors.white),
+              ),
+              Icon(
+                Icons.edit_outlined,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.account_circle_outlined,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+        centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(56),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide.none),
+                contentPadding: EdgeInsets.zero,
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
